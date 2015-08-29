90aac0e933c9bf6035e2bb59398f1f9a82cbe53d
krupin.dv <krupin.dv19@gmail.com>
2015-08-29 08:26:39 +0300

    EventAdmin now only shows active users

--- training/admin.py
+++ training/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-
-
-
 from django.contrib import admin
+
+from django.contrib.auth.models import User
 from training.models import Event, Score
+
+
 
 
 
@@ -14,10 +15,15 @@ class ScoreInline(admin.TabularInline):
     ordering = ['user']
 
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user":
+            kwargs["queryset"] = User.objects.filter(is_active=True).order_by("username")
+        return super(ScoreInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'users_count')
-    
+
     inlines = [ScoreInline]
 
 
