8c9fd192d296721b4cd9a7ae7bbe7b4a9a940673
krupin.dv <krupin.dv19@gmail.com>
2015-08-29 11:08:48 +0300

    settings.ALLOWED_HOST fix, STATIC_ROOT changed

--- mech/settings.py
+++ mech/settings.py
@@ -49,7 +49,7 @@ DEFAULT_FROM_EMAIL = "contact@swordsman.su"
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["http://swordsman.su"]
+ALLOWED_HOSTS = ["swordsman.su", ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -88,7 +88,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(STATIC_PATH, "root_static")
+STATIC_ROOT = os.path.join(STATIC_PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
