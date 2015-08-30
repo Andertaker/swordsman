3acb3ca9f1f973f28c4272ae204dbffd96afe788
krupin.dv <krupin.dv19@gmail.com>
2015-08-30 14:32:26 +0300

    "www.swordsman.su" was added to settings.ALLOWED_HOSTS"

--- mech/settings.py
+++ mech/settings.py
@@ -49,7 +49,7 @@ DEFAULT_FROM_EMAIL = "contact@swordsman.su"
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["swordsman.su", ]
+ALLOWED_HOSTS = ["swordsman.su", "www.swordsman.su", ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
